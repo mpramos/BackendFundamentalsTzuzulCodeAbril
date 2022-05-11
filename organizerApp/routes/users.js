ab456bcdb8805a1fa85b6b145cc90d93bb1e3c5a
mpramos <mpramos78915400@gmail.com>
2022-05-11 09:09:32 -0400

    Introducción a promesas

--- organizerApp/routes/users.js
+++ organizerApp/routes/users.js
@@ -4,12 +4,14 @@ const database=require('../libs/database')
 const router =express.Router()
 console.log(database)
 router.get("/users",(req,res)=>{
-    database.connection.query("SELECT * FROM users",function (error,result) {
-        console.log(error)
-        console.log(result)
-    })
-    res.json({
-        ruta:"users"
+    database.connection.query("SELECT * FROM user",function (error,result) {
+        if(error!=null){
+            res.json({
+                message:error.sqlMessage
+            })
+
+        }
+        res.json(result)
     })
 })
 router.get("/login",(req,res)=>{
